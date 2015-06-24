197ec007d7b9717eac4a2e6c36fc101ae250da6b
tim <timlyomaddison@gmail.com>
2015-06-24 22:41:39 +0100

    Initial commit

---
+++ scanCode.py
@@ -0,0 +1,10 @@
+
+def key_from_code(code):
+    """ get the name of the button from the numerical key
+    :param code: int of key code
+    :type code: int
+    :return: button name
+    :rtype: str
+    """
+
+def _find
